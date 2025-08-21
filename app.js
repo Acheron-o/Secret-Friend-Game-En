8b72ed0baddb40704e67ba940eb8d5be8be6ee12
Acheron-o <caio.dev.person@gmail.com>
2025-08-21 00:40:39 -0300

    Commiting all my previous files from (amigo secreto game) but now in english

---
+++ app.js
@@ -0,0 +1,71 @@
+// The main goal of this challenge is to strengthen your programming logic skills.
+// Here you must develop the logic to solve the Secret Friend problem.
+
+// Create an array to store the names
+let players = [];
+
+// Function to add friends
+function addFriend() {
+    const input = document.getElementById('friend');
+    let name = input.value.trim();
+
+    // Check if input is empty
+    if (name === "") { 
+        displayMessageOnScreen("h2", "Please enter a valid name!");
+        return;
+    } 
+
+    displayMessageOnScreen("h2", "Enter your friends' names");
+
+    players.push(name);
+   
+    // Clear input field
+    name = document.querySelector("input");
+    name.value = "";
+
+    // Update the list on screen
+    const list = document.getElementById('friendList');
+    list.innerHTML = ""; // Clear before updating
+    players.forEach(friend => {
+        const li = document.createElement('li');
+        li.textContent = friend;
+        list.appendChild(li);
+    });
+
+    result.innerHTML = "";
+}
+
+// Function to generate a random friend
+function randomFriend(totalFriends) {
+  return Math.floor(Math.random() * totalFriends);
+}
+
+// Function to draw a secret friend
+function drawFriend() {
+    if (players.length < 2){
+        displayMessageOnScreen("h2", "At least 2 friends are required to perform the draw.");
+        return;
+    }
+    else {
+        let drawResult = randomFriend(players.length);
+        const drawnFriend = players[drawResult];
+
+        // Show the result on screen
+        const result = document.getElementById('result');
+        result.innerHTML = ""; // Clear previous result
+        const li = document.createElement('li');
+        li.textContent = drawnFriend;
+        result.appendChild(li);
+
+        // Clear the players list
+        players = [];
+        document.getElementById('friendList').innerHTML = "";
+
+        displayMessageOnScreen("h2", "Draw completed successfully! Add new friends to perform another draw.");    
+    }
+}
+
+// Function to display messages dynamically
+function displayMessageOnScreen(tag, message) {
+    document.querySelector(tag).innerHTML = message;
+}
